f73adfbedff4ce5c59a28f262102325f9847772f
Thibaut Septon <tsepton@gmail.com>
2024-11-13 14:05:35 +0100

    Update exposed data types with zod schema

--- src/types/exposed.ts
+++ src/types/exposed.ts
@@ -1,13 +1,17 @@
+import { z } from "zod";
 
-export interface StarePoint {
-    x: number;
-    y: number;
-}
+export const StarePointSchema = z.object({
+  x: z.number(),
+  y: z.number(),
+});
+export type StarePoint = z.infer<typeof StarePointSchema>;
 
-export interface Question {
-  query: string;
-  image: string;
-  gaze: StarePoint[];
-}
+export const QuestionSchema = z.object({
+  query: z.string().trim().min(1, { message: "Required" }),
+  image: z.string().trim().min(1, { message: "Required" }),
+  gaze: z.array(StarePointSchema),
+});
+export type Question = z.infer<typeof QuestionSchema>;
 
+// TODO: determine Answer type
 export interface Answer {}
