32ea7edfd177374098d7d47ee37b53aa2e37910e
Thibaut Septon <tsepton@gmail.com>
2024-11-13 14:05:17 +0100

    Add Either type

--- src/types/internal.ts
+++ src/types/internal.ts
@@ -12,3 +12,38 @@ export interface ProcessedQuestion {
   image: Image;
   gaze: AggregatedStarePoint[];
 }
+
+// Base Either type
+export abstract class Either<L, R> {
+  abstract match<T>(onLeft: (left: L) => T, onRight: (right: R) => T): T;
+}
+
+// Left variant
+export class Left<L, R> extends Either<L, R> {
+  constructor(public left: L) {
+    super();
+  }
+
+  match<T>(onLeft: (left: L) => T, _: (right: R) => T): T {
+    return onLeft(this.left);
+  }
+}
+
+// Right variant
+export class Right<L, R> extends Either<L, R> {
+  constructor(public right: R) {
+    super();
+  }
+
+  match<T>(_: (left: L) => T, onRight: (right: R) => T): T {
+    return onRight(this.right);
+  }
+}
+
+export function createLeft<L, R>(left: L): Either<L, R> {
+  return new Left(left);
+}
+
+export function createRight<L, R>(right: R): Either<L, R> {
+  return new Right(right);
+}
