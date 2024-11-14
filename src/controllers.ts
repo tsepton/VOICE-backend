2992b448a53e70538c3342689b0b388b379470f4
Thibaut Septon <tsepton@gmail.com>
2024-11-14 14:09:47 +0100

    Add suport for Websocket connection

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,10 +1,31 @@
-import { Express, Request } from "express";
+import { Express, json, Request, urlencoded } from "express";
+import { WebSocketServer } from "ws";
 import * as domain from "./domain.ts";
 import { Question } from "./types/exposed.ts";
 import { tryCatch } from "./types/internal.ts";
 import { process } from "./validators.ts";
 
-export default function initControllers(app: Express) {
+export function initHttp(app: Express) {
+
+  // Configurations
+  app.use(json({limit: '200mb'}));
+  app.use(urlencoded({ extended: true, limit: '200mb' }));
+  
+  // Middlewares
+  app.use((req, res, next) => {
+    console.log(`Route accessed: ${req.method} ${req.originalUrl}`);
+    // console.log(`Request body: ${JSON.stringify(req.body)}`);
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    res.on('finish', () => {
+      console.log(`Response sent: ${req.method} ${req.originalUrl} - Status: ${res.statusCode}`);
+    });
+    next();
+  });
+  
+  // Routes
   app.get("/", (_, res) => {
     res.status(200).json({ message: "Server is up and running!" });
   });
@@ -25,5 +46,23 @@ export default function initControllers(app: Express) {
         );
       }
     );
+  });
+}
+
+export function initWebsocket(wss: WebSocketServer) {
+  wss.on("connection", (ws, req) => {
+    console.log(`WebSocket connection established on ${req.url}`);
+  
+    ws.on("message", (message) => {
+      console.log(`Received WebSocket message on ${req.url}:`, message);
+  
+      // TODO - Implement WebSocket message handling
+
+      ws.send(`Echo: ${message}`);
+    });
+  
+    ws.on("close", () => {
+      console.log(`WebSocket connection closed on ${req.url}`);
+    });
   });
 }
