6d54426286e079c7a05ab186cb757bfec4bab9ce
Thibaut Septon <tsepton@gmail.com>
2024-11-14 12:13:10 +0100

    Update prompt

--- src/libs/assistant/ollama.ts
+++ src/libs/assistant/ollama.ts
@@ -1,10 +1,9 @@
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 import { ChatOllama } from "@langchain/ollama";
 import { Assistant } from "./assistant.ts";
-
 export default class OllamaAssistant implements Assistant {
   private _llm = new ChatOllama({
-    model: OllamaVisionModel.llava_llama3,
+    model: OllamaVisionModel.llama32_vision,
     temperature: 0,
     maxRetries: 2,
     baseUrl: `${process.env.OLLAMA_HOST ?? "http://0.0.0.0"}:${
@@ -19,12 +18,17 @@ export default class OllamaAssistant implements Assistant {
   ): Promise<string> {
     const system = [
       "You are an assistant for question-answering tasks. " +
-      "Use the following pieces of retrieved context to answer the question. " +
-      "If you don't know the answer, say that you don't know. " +
-      "Use three sentences maximum and keep the answer concise. ",
-      "\n \n",
-      "{context}"
-    ].join(" ");
+        "Use the provided images as context to answer the question. " +
+        "If you don't know the answer, say that you don't know. " +
+        "Use three sentences maximum and keep the answer concise. ",
+      "Images you receive are always in pairs: the original image and a modified version with a heatmap overlay. " +
+        "The heatmap is built using user gaze data. " +
+        "Brighter areas are the one where user laid his eyes on and therefore " +
+        "should be used to disambiguate pronouns in the user question or request. " +
+        "Pronouns may not refer to the most prominent object in the image. " +
+        "Give more importance to objects under the brighter areas for pronoun disambiguation. ",
+      "Do not describe the image, answer the question. ",
+    ].join("\n\n");
 
     const images = [original, heatmap].map((uri) => ({
       type: "image_url",
