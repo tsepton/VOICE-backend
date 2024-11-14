1929d0ac1817a857a526b5370a3b60bb34c85518
Thibaut Septon <tsepton@gmail.com>
2024-11-14 11:17:19 +0100

    Add Ollama Assistant class

--- src/libs/assistant/assistant.ts
+++ src/libs/assistant/assistant.ts
@@ -1,15 +1,9 @@
-
-enum VisionModel {
-  moondream = "moondream",
-  llava_llama3 = "llava-llama3",
-  llama32_vision = "llama3.2-vision",
-  bakllava = "bakllava",
-}
+import OllamaAssistant from "./ollama.ts";
 
 export interface Assistant {
   prompt: (query: string, original: string, heatmap: string) => Promise<string>;
-  model: VisionModel;
 }
 
+const factory: Assistant = new OllamaAssistant();
 
-export default undefined as any as Assistant;
+export default factory;
