d2ac47b4d63d310aeb66638fb7a8e554140f06ea
Thibaut Septon <tsepton@gmail.com>
2024-11-18 10:40:40 +0100

    Add OpenAI zero shot prompting

---
+++ src/libs/assistant/zero-shot-learning.ts
@@ -0,0 +1,40 @@
+import { BaseChatModel } from "@langchain/core/language_models/chat_models";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import { Assistant } from "./assistant.ts";
+
+export abstract class BaseZeroShotLearning implements Assistant {
+  protected abstract _llm: BaseChatModel;
+
+  protected abstract _system: string;
+
+  public async prompt(
+    query: string,
+    original: string,
+    heatmap: string
+  ): Promise<string> {
+
+    const images = [original, heatmap].map((uri) => ({
+      type: "image_url",
+      image_url: {
+        url: uri,
+      },
+    }));
+
+    const answer = await this._llm.invoke([
+      new SystemMessage({
+        content: this._system,
+      }),
+      new HumanMessage({
+        content: [
+          {
+            type: "text",
+            text: query,
+          },
+          ...images,
+        ],
+      }),
+    ]);
+
+    return answer.content as string; // FIXME - temporary
+  }
+}
